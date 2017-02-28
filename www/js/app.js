02f65b7b5040cca520dd4c037edef0edd107eb9c
Pepe Valdivia <jvaldivia@softweb.pe>
2017-02-28 12:43:55 -0500

    Validación de formulario del login

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,13 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'LoginModule'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -29,13 +20,13 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         url: '/app',
         abstract: true,
         templateUrl: 'js/app/menu/menu.html',
-        controller: 'AppCtrl'
     })
 
     .state('login', {
         url: '/login',
         abstract: false,
         templateUrl: 'js/app/login/login.html',
+        controller: 'LoginController'
     })
 
     .state('registro', {
@@ -58,45 +49,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
             }
         }
     })
-  /*
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  */
-  // if none of the above states are matched, use this as the fallback
-  //$urlRouterProvider.otherwise('/app/about');
   $urlRouterProvider.otherwise('/login');
 });
---
+++ www/js/app/login/LoginController.js
@@ -0,0 +1,44 @@
+angular.module('LoginModule')
+
+.controller('LoginController', function($scope){
+	$scope.usuario = {};
+
+	$scope.$on('$ionicView.loaded', function(){
+
+	});
+
+	$scope.validarForm = function(usuario){
+		var validado = false;
+
+		if (typeof $scope.usuario.usuario === 'undefined'){
+			$("#txtUsuario").parent().addClass("input-text-error");
+			$("#txtUsuario").addClass('input-text-error-placeholder');
+			$("#txtUsuario").val("");
+           $("#txtUsuario").attr("placeholder", "Ingrese su usuario");
+			validado = false;
+		}else{
+			$("#txtUsuario").parent().removeClass("input-text-error");
+			$("#txtUsuario").removeClass('input-text-error-placeholder');
+			validado = true;
+		}
+
+		if (typeof $scope.usuario.contrasenia === 'undefined'){
+			$("#txtContrasenia").parent().addClass("input-text-error");
+			$("#txtContrasenia").addClass('input-text-error-placeholder');
+			$("#txtContrasenia").val("");
+           $("#txtContrasenia").attr("placeholder", "Ingrese su contraseña");
+			validado = false;
+		}else{
+			$("#txtContrasenia").parent().removeClass("input-text-error");
+			$("#txtContrasenia").removeClass('input-text-error-placeholder');
+			validado = true;
+		}
+
+		return validado;
+	}
+
+	$scope.validarUsuario = function(usuario){
+		console.log("validarUsuario");
+		return true;
+	}
+});
